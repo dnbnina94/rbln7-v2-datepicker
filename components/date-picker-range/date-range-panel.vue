2f270ef58853dc7ce53e8693acf3a86a91f8aead
Nina <nina.grujic.94@gmail.com>
2019-10-17 22:55:34 +0200

    polish

--- components/date-picker-range/date-range-panel.vue
+++ components/date-picker-range/date-range-panel.vue
@@ -1,11 +1,11 @@
 <template>
     <transition name="zoom-in-top">
+        <div class="mobile-dateinfo-wrapper" v-show="shown">
+            <div class="date-info"></div>
+            <div class="cancel-button" @click="shown = false">Cancel</div>
+        </div>
         <div class="v2-picker-panel-wrap-outer-1" v-show="shown">
             <div class="v2-picker-panel-wrap-outer-2">
-            <div class="mobile-dateinfo-wrapper" v-show="shown">
-                <div class="date-info"></div>
-                <div class="cancel-button" @click="shown = false">Cancel</div>
-            </div>
             <div class="v2-picker-panel-wrap v2-picker-range-panel-wrap" ref="panel" v-show="shown" :style="position">
                 <short-cuts v-if="shownSideBar" :shortcuts="pickerOptions.shortcuts" @shortcut-click="handleShortcutPick"></short-cuts>
                 <div class="v2-picker-range-panel v2-picker-range__left-panel" :style="{marginLeft: shownSideBar ? '110px' : '0'}">
